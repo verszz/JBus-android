b8c1f51e908ed04b18fa0c72151d06f10d81c55b
verszz <zikri.zulfa@ui.ac.id>
2023-11-23 21:25:36 +0700

    TP09-Pengerjaan TP09

--- app/src/main/java/com/zikriZulfaAzhimJBusRS/jbus_android/request/BaseApiService.java
+++ app/src/main/java/com/zikriZulfaAzhimJBusRS/jbus_android/request/BaseApiService.java
@@ -4,8 +4,9 @@ import com.zikriZulfaAzhimJBusRS.jbus_android.model.Account;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
-
+import retrofit2.http.Query;
 public interface BaseApiService {
     @GET("account/{id}")
     Call<Account> getAccountbyId (@Path("id") int id);
